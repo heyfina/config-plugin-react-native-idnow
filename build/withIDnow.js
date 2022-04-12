417389187ae04dd64ba4c6529aa828c1c459d7b0
mortadha fadhlaoui <mortadhafadhlaoui@gmail.com>
2022-04-12 14:07:44 +0200

    Fix cannot invoke method android() on null object

--- build/withIDnow.js
+++ build/withIDnow.js
@@ -8,39 +8,39 @@ const { getMainApplicationOrThrow } = config_plugins_1.AndroidConfig.Manifest;
 // updating ios...
 const withPodfileUpdate = (config) => {
     return (0, config_plugins_1.withDangerousMod)(config, [
-        "ios",
+        'ios',
         async (config) => {
             await editPodfile(config, (podfile) => {
                 // Fix for an error taken from here
                 // https://dev.to/kylefoo/xcode-12-new-build-system-warns-multiple-commands-produce-assets-car-56im
                 // solution (2)
-                podfile = addLines(podfile, "platform :ios", 1, [
+                podfile = addLines(podfile, 'platform :ios', 1, [
                     "install! 'cocoapods', :disable_input_output_paths => true",
                     "plugin 'cocoapods-user-defined-build-types'",
-                    "",
-                    "enable_user_defined_build_types!",
+                    '',
+                    'enable_user_defined_build_types!',
                 ]);
-                podfile = addLines(podfile, "config = use_native_modules!", 1, [
+                podfile = addLines(podfile, 'config = use_native_modules!', 1, [
                     "  pod 'IDnowSDK', '5.1.6', :build_type => :static_framework",
                     "  pod 'AFNetworking', '4.0.1', :modular_headers => true",
                     "  pod 'FLAnimatedImage', '1.0.16', :modular_headers => true",
                     "  pod 'libPhoneNumber-iOS', '0.9.15', :modular_headers => true",
                     "  pod 'Masonry', '1.1.0', :modular_headers => true",
                     "  pod 'SocketRocket', '0.5.1', :modular_headers => true",
-                    "",
+                    '',
                 ]);
                 //Fix from https://github.com/idnow/de.idnow.ios#cocoapods--xcode-9
-                podfile = addLines(podfile, "react_native_post_install", 2, [
-                    "",
+                podfile = addLines(podfile, 'react_native_post_install', 2, [
+                    '',
                     // 'post_install do |installer|', // this was used before a similar section started being included on the podFile
                     `    copy_pods_resources_path = "Pods/Target Support Files/Pods-${config.modRequest.projectName}/Pods-${config.modRequest.projectName}-resources.sh"`,
-                    "    string_to_replace = 'if [[ $line != \"${PODS_ROOT}*\" ]]; then'",
+                    '    string_to_replace = \'if [[ $line != "${PODS_ROOT}*" ]]; then\'',
                     '    assets_compile_with_app_icon_arguments = \'if [[ $line != "${PODS_ROOT}*" && $line != *"Sample"* ]]; then\'',
-                    "    text = File.read(copy_pods_resources_path)",
-                    "    new_contents = text.gsub(string_to_replace, assets_compile_with_app_icon_arguments)",
+                    '    text = File.read(copy_pods_resources_path)',
+                    '    new_contents = text.gsub(string_to_replace, assets_compile_with_app_icon_arguments)',
                     '    File.open(copy_pods_resources_path, "w") {|file| file.puts new_contents }',
                     // 'end', // this was used before a similar section started being included on the podFile
-                    "",
+                    '',
                 ]);
                 return podfile;
             });
@@ -49,32 +49,32 @@ const withPodfileUpdate = (config) => {
     ]);
 };
 async function editPodfile(config, action) {
-    const podfilePath = (0, path_1.join)(config.modRequest.platformProjectRoot, "Podfile");
+    const podfilePath = (0, path_1.join)(config.modRequest.platformProjectRoot, 'Podfile');
     try {
         const podfile = action(await readFileAsync(podfilePath));
         return await saveFileAsync(podfilePath, podfile);
     }
     catch (e) {
-        config_plugins_1.WarningAggregator.addWarningIOS("idnow", `Couldn't modified Podfile - ${e}.`);
+        config_plugins_1.WarningAggregator.addWarningIOS('idnow', `Couldn't modified Podfile - ${e}.`);
     }
 }
 async function readFileAsync(path) {
-    return fs_1.promises.readFile(path, "utf8");
+    return fs_1.promises.readFile(path, 'utf8');
 }
 async function saveFileAsync(path, content) {
-    return fs_1.promises.writeFile(path, content, "utf8");
+    return fs_1.promises.writeFile(path, content, 'utf8');
 }
 function addLines(content, find, offset, toAdd) {
-    const lines = content.split("\n");
+    const lines = content.split('\n');
     let lineIndex = lines.findIndex((line) => line.match(find));
     for (const newLine of toAdd) {
         lines.splice(lineIndex + offset, 0, newLine);
         lineIndex++;
     }
-    return lines.join("\n");
+    return lines.join('\n');
 }
-const ERROR_MSG_PREFIX = "An error occurred while configuring iOS project. ";
-const filePaths = ["./plugins/idnow/RNIdnow"];
+const ERROR_MSG_PREFIX = 'An error occurred while configuring iOS project. ';
+const filePaths = ['./plugins/idnow/RNIdnow'];
 const withXCodeProjectUpdate = (config) => {
     return (0, config_plugins_1.withXcodeProject)(config, (config) => {
         addRNIdNowFiles(config.modRequest.projectRoot, {
@@ -92,14 +92,14 @@ function addRNIdNowFiles(projectRoot, { filePaths, project, projectName, }) {
     const sourceRoot = config_plugins_1.IOSConfig.Paths.getSourceRoot(projectRoot);
     for (const fileRelativePath of filePaths) {
         addMFile({
-            fileRelativePath: fileRelativePath + ".m",
+            fileRelativePath: fileRelativePath + '.m',
             projectRoot,
             sourceRoot,
             project,
             projectName,
         });
         addHFile({
-            fileRelativePath: fileRelativePath + ".h",
+            fileRelativePath: fileRelativePath + '.h',
             projectRoot,
             sourceRoot,
             project,
@@ -113,7 +113,7 @@ const addMFile = (file) => {
     let { fileRelativePath, projectRoot, sourceRoot, project, projectName } = file;
     const fileName = (0, path_1.basename)(fileRelativePath);
     const sourceFilepath = (0, path_1.resolve)(projectRoot, fileRelativePath);
-    const destinationFilepath = (0, path_1.resolve)(sourceRoot, "..", fileName);
+    const destinationFilepath = (0, path_1.resolve)(sourceRoot, '..', fileName);
     (0, fs_1.copyFileSync)(sourceFilepath, destinationFilepath);
     if (!project.hasFile(`${projectName}/${fileName}`)) {
         project = config_plugins_1.IOSConfig.XcodeUtils.addBuildSourceFileToGroup({
@@ -127,7 +127,7 @@ const addHFile = (file) => {
     let { fileRelativePath, projectRoot, sourceRoot, project, projectName } = file;
     const fileName = (0, path_1.basename)(fileRelativePath);
     const sourceFilepath = (0, path_1.resolve)(projectRoot, fileRelativePath);
-    const destinationFilepath = (0, path_1.resolve)(sourceRoot, "..", fileName);
+    const destinationFilepath = (0, path_1.resolve)(sourceRoot, '..', fileName);
     (0, fs_1.copyFileSync)(sourceFilepath, destinationFilepath);
     if (!project.hasFile(`${projectName}/${fileName}`)) {
         project = config_plugins_1.IOSConfig.XcodeUtils.addFileToGroupAndLink({
@@ -147,20 +147,20 @@ const applyManifestConfig = async (config, androidManifest) => {
     const mainApplication = getMainApplicationOrThrow(androidManifest);
     const manifest = androidManifest.manifest;
     const xmlnsTools = {
-        "xmlns:tools": "http://schemas.android.com/tools",
+        'xmlns:tools': 'http://schemas.android.com/tools',
     };
     const toolsReplace = {
-        "tools:replace": "android:name,android:icon,android:theme,android:allowBackup",
+        'tools:replace': 'android:name,android:icon,android:theme,android:allowBackup',
     };
     // Add xmlns:tools attribute to the manifest tag
-    if (!manifest.$.hasOwnProperty("xmlns:tools")) {
+    if (!manifest.$.hasOwnProperty('xmlns:tools')) {
         manifest.$ = {
             ...manifest.$,
             ...xmlnsTools,
         };
     }
     // Add tools:replace attribute to the application tag
-    if (!mainApplication.$.hasOwnProperty("tools:replace")) {
+    if (!mainApplication.$.hasOwnProperty('tools:replace')) {
         mainApplication.$ = {
             ...mainApplication.$,
             ...toolsReplace,
@@ -214,7 +214,7 @@ const applyRepositories = (appBuildGradle) => {
 };
 //exclude section
 const applyPackagingOptionsAndConfigurations = (appBuildGradle) => {
-    const idnowPackagingOptions = `android {
+    const idnowPackagingOptions = `\nandroid {
     packagingOptions {
         exclude "org.bouncycastle"
         exclude 'org/bouncycastle/x509/CertPathReviewerMessages_de.properties'
@@ -270,4 +270,4 @@ const withIDnow = (expoConfig) => {
     expoConfig = withXCodeProjectUpdate(expoConfig);
     return expoConfig;
 };
-exports.default = (0, config_plugins_1.createRunOncePlugin)(withIDnow, "IDNowSDK", "1.0.2");
+exports.default = (0, config_plugins_1.createRunOncePlugin)(withIDnow, 'IDNowSDK', '1.0.2');
